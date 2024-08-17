92cf888aa1b36a3874973a516a5b10fa6e14a252
AtiqurRahmanAtik <atikahmedtanvir80@gmail.com>
2024-08-17 15:01:47 +0600

    complet previous button and next button functionality

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -138,9 +138,12 @@ const Home = () => {
         </div>
 
 
-        {/* page button  */}
+        {/* previous button  */}
         <div className='flex justify-center mt-12'>
-        <button className='px-4 py-2 mx-1 bg-green-400 text-gray-700 disabled:text-gray-500 capitalize  rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
+
+        <button disabled={currentPage === 1}
+        onClick={()=> handlePaginationButton(currentPage -1)}
+         className='px-4 py-2 mx-1 bg-green-400 text-gray-700 disabled:text-gray-500 capitalize  rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
           <div className='flex items-center -mx-1'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -167,7 +170,7 @@ const Home = () => {
           <button
           onClick={()=> handlePaginationButton(btnNum)}
             key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            className={`hidden ${currentPage === btnNum ? 'bg-blue-500 text-white ': ''} px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
           >
             {btnNum}
           </button>
@@ -175,7 +178,9 @@ const Home = () => {
 
 
         {/* next button */}
-        <button className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-green-400  rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
+        <button disabled={currentPage === numberOfPages}
+        onClick={()=> handlePaginationButton(currentPage + 1)}
+        className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-green-400  rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
           <div className='flex items-center -mx-1'>
             <span className='mx-1 '>Next</span>
 
